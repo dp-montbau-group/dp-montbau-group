826f0e5c7a1ccf188cafcc03b93af1062c7b5a48
dp-montbau-group <stehno.richard96@gmail.com>
2026-09-13 18:11:10 +0200

    Add translations for Czech, English, and German

--- js/translations.js
+++ js/translations.js
@@ -1,1 +1,32 @@
 
+const translations = {
+  cs: {
+    navHome: "Úvod",
+    navAbout: "O nás",
+    navServices: "Služby",
+    navProjects: "Realizace",
+    navReferences: "Reference",
+    navContact: "Kontakt",
+    navCta: "Nezávazná poptávka"
+  },
+
+  en: {
+    navHome: "Home",
+    navAbout: "About us",
+    navServices: "Services",
+    navProjects: "Projects",
+    navReferences: "References",
+    navContact: "Contact",
+    navCta: "Request a quote"
+  },
+
+  de: {
+    navHome: "Startseite",
+    navAbout: "Über uns",
+    navServices: "Leistungen",
+    navProjects: "Projekte",
+    navReferences: "Referenzen",
+    navContact: "Kontakt",
+    navCta: "Unverbindliche Anfrage"
+  }
+};
